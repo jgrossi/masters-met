755f50bcd8e4f45a9ca8bd5ae5728053a87c4c91
Junior Grossi <juniorgro@gmail.com>
2015-05-06 19:06:03 -0300

    Changed individual table layout

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-	
+	$('.table-result a.area').click(function(e) {
+		e.preventDefault();
+		var tr = $(this).closest('tr');
+		var trNumber = tr.data('tr');
+		var selector = ".table-result tbody.tr-group[data-tr=" + trNumber + "]";
+		$(selector).toggle(); 
+	});
 });
--- public/individual.php
+++ public/individual.php
@@ -6,33 +6,58 @@
 		<a href="extraction.php"><?php echo uniqid() ?></a><br>
 		<span class="text-muted">Masters Collection #1</span>
 	</p>
-	<table class="table table-bordered table-striped table-result table-individual">
-		<thead>
-			<tr>
-				<th width="16.67%">Paper</th>
-				<th width="16.67%">Title</th>
-				<th width="16.67%">Authores</th>
-				<th width="16.67%">E-mails</th>
-				<th width="16.67%">Abstract</th>
-				<th width="16.67%">References</th>
-			</tr>
-		</thead>
-		<tbody>
-		<?php foreach(range(1,100) as $row): ?>
-			<tr>
-				<td><strong>#<?php echo $row ?></strong></td>
-				<td><?php echo number_format(rand(9400, 10000)/100, 2) ?>%</td>
-				<td><?php echo number_format(rand(9400, 10000)/100, 2) ?>%</td>
-				<td><?php echo number_format(rand(9400, 10000)/100, 2) ?>%</td>
-				<td><?php echo number_format(rand(9400, 10000)/100, 2) ?>%</td>
-				<td><?php echo number_format(rand(9400, 10000)/100, 2) ?>%</td>
-			</tr>
-		<?php endforeach; ?>
-		</tbody>
-	</table>
+	
+	<?php $areas = ['Computer Science', 'Biological', 'Music', 'Information Science', 'Civil Engineer', 'Sociology', 'Psicology', 'Another Area Name', 'Something Engineer', 'Medicine'] ?>
+	<?php $area = 0 ?>
+
+	<?php foreach(range(1,100) as $row): ?>
+		
+		<?php if (($row-1) % 10 == 0): ?>
+			<p class="lead"><?php echo $areas[$area] ?></p>
+			<table class="table table-bordered table-striped table-result table-individual">
+				<thead>
+					<tr>
+						<th width="16.67%">Paper</th>
+						<th width="16.67%">Title</th>
+						<th width="16.67%">Authors</th>
+						<th width="16.67%">E-mails</th>
+						<th width="16.67%">Abstract</th>
+						<th width="16.67%">References</th>
+					</tr>
+				</thead>
+				<tbody>
+		<?php endif ?>
+
+					<tr>
+						<td class=""><strong>#<?php echo $row ?></strong></td>
+						<td><?php echo number_format(rand(9400, 10000)/100, 2) ?>%</td>
+						<td><?php echo number_format(rand(9400, 10000)/100, 2) ?>%</td>
+						<td><?php echo number_format(rand(9400, 10000)/100, 2) ?>%</td>
+						<td><?php echo number_format(rand(9400, 10000)/100, 2) ?>%</td>
+						<td><?php echo number_format(rand(9400, 10000)/100, 2) ?>%</td>
+					</tr>
+
+		<?php if ($row%10==0): ?>
+					<tr class="">
+						<td class="metadata">Average</td>
+						<td class="metadata"><?php echo number_format(rand(9400, 10000)/100, 2) ?>%</td>
+						<td class="metadata"><?php echo number_format(rand(9400, 10000)/100, 2) ?>%</td>
+						<td class="metadata"><?php echo number_format(rand(9400, 10000)/100, 2) ?>%</td>
+						<td class="metadata"><?php echo number_format(rand(9400, 10000)/100, 2) ?>%</td>
+						<td class="metadata"><?php echo number_format(rand(9400, 10000)/100, 2) ?>%</td>
+					</tr>
+				</tbody>
+			</table>
+			<?php $area++ ?>
+		<?php endif ?>
+
+	<?php endforeach; ?>
+
+	<hr>
+	
 	<table class="table table-bordered table-result font-lg">
-		<tr class="active">
-			<td width="16.67%" class="metadata">Average</td>
+		<tr class="warning">
+			<td width="16.67%" class="metadata">Total</td>
 			<td width="16.67%" class="metadata"><?php echo number_format(rand(9400, 10000)/100, 2) ?>%</td>
 			<td width="16.67%" class="metadata"><?php echo number_format(rand(9400, 10000)/100, 2) ?>%</td>
 			<td width="16.67%" class="metadata"><?php echo number_format(rand(9400, 10000)/100, 2) ?>%</td>
@@ -40,6 +65,7 @@
 			<td width="16.67%" class="metadata"><?php echo number_format(rand(9400, 10000)/100, 2) ?>%</td>
 		</tr>
 	</table>
+	<hr>
 	<nav>
 		<ul class="pager">
 			<li class="previous disabled"><a href="#"><span aria-hidden="true">&larr;</span> CiteSeer</a></li>
